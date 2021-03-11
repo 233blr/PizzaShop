7b43ae034103bcbcc6f6c6d2c199e79dfb178335
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-03-11 14:53:24 +0300

    Added new component PizzaBlock

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Header/Button';
 import Categories from './Header/Categories';
 import Header from './Header';
 import SortPopup from './Header/SortPopup';
+import PizzaBlock from './PizzaBlock';
 
-export { Button, Categories, Header, SortPopup };
+export { Button, Categories, Header, SortPopup, PizzaBlock };
