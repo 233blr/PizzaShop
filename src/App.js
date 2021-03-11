fb5f999bf0f346dbf6577761e9f048937a6df55f
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-03-11 14:52:32 +0300

    Added get request

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
-
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    axios.get('https://pizzashop-76313-default-rtdb.firebaseio.com/pizzas.json')
+      .then((res => setPizzas(res.data)))
+      .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home pizzas={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
